e9bb7ecf918d94de17041fcf5a87a89e13e0b820
hatem <hatem@LAPTOP-0NGITM3D>
2022-09-13 13:09:00 +0200

    Add project files.

---
+++ Kyhloopardemo/App.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kyhloopardemo
+{
+    internal class App
+    {
+        public void Run() 
+        {
+            Console.WriteLine("mata in två tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+            int summa = tal1 + tal2;
+            Console.WriteLine("summan är" + summa);
+
+            Console.WriteLine("vill du fortsätta J/N");
+
+            if(i == "J")
+            {
+                Console.WriteLine("");
+            }
+
+
+
+
+
+
+            /*int tal1 = Convert.ToInt32((Console.ReadLine() ));
+            int tal2 = Convert.ToInt32((Console.ReadLine()));
+
+            /* for(int i = tal1+1; i < tal2; i++)
+             {
+                 Console.WriteLine(i);
+             }*/
+           /* tal1 = tal1 + 1;
+                
+                while (tal1 < tal2) 
+                {
+                  Console.WriteLine(tal1);
+                     tal1++;
+
+                 }*/
+
+                /*if(tal1 > tal2)
+            {
+                for(int i = tal1-1; i > tal2; i--)
+                {
+                    Console.WriteLine(i);
+                }*/
+                
+            }
+
+            
+
+
+
+           /* for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int h = 0;
+            while(h <= 10)
+            {
+                Console.WriteLine(h);
+                h++;
+            }*/
+        }
+
+
+
+
+    }
---
+++ Kyhloopardemo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Kyhloopardemo;
+var app = new App();
+app.Run();
